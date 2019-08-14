6d068b719a7fce821bbb58f825cf7a5314ee485e
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-14 21:32:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,13 +6,13 @@ end
 
 def find_max_value(array)
   array.each do |max|
-    puts array
+    puts max
 end
 end
 
   def find_min_value(array)
     array.each do |min|
-      puts array
+      puts min
     end
   
 end
