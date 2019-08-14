f664a5dd08560a4765c3ba7871063f512b950325
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-14 22:04:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
    i = 0
    array.each do |element|
-   if element == value_to_find
-     return i
-   end
-   i += 1
-end
+     if element == value_to_find
+       return i
+     end
+     i += 1
+  end
 nil
 end
 
