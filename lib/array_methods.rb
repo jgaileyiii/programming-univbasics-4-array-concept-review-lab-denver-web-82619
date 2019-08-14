720d21589bf51dda188d69b66140ab75de9ec832
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-14 21:29:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,9 +4,8 @@ def find_element_index(array, value_to_find)
 end
 end
 def find_max_value(array)
-  array.each do |max|
-    puts array.max
-  end
+  array.max do |index|
+    puts index
 end
 
   def find_min_value(array)
